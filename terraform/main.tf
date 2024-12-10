cc7fbe9a1e393dd636d09eb062cd4341f979788c
Mtiri Wissem <wissem.mtiri@insat.ucar.tn>
2024-12-10 23:09:12 +0100

    rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "linux_webapp" {
-  name                = "tf-linux-webapp"
+  name                = "MythoMorphosis-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.asp.id
