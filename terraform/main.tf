395c4d35d33982141d7ee9e4684b7b03b082360c
Mtiri Wissem <wissem.mtiri@insat.ucar.tn>
2024-12-10 23:16:14 +0100

    rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@
 #================================================================================================
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "terraform-app-service-rg"
+  name     = "automation-rg"
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -32,7 +32,7 @@ resource "azurerm_service_plan" "asp" {
 }
 
 resource "azurerm_linux_web_app" "linux_webapp" {
-  name                = "terraform-webapp"
+  name                = "auto-terraform-webapp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.asp.id
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,14 +7,3 @@ output "web_app_container" {
   value       = azurerm_linux_web_app.linux_webapp.site_config[0].linux_fx_version
   description = "The Docker image version being used by the web app"
 }
-
-output "jenkins_ip" {
-  value       = azurerm_public_ip.public-ip.ip_address
-  description = "The public IP address of the Jenkins node"
-}
-
-output "jenkins_ssh_key" {
-  value       = tls_private_key.ssh.private_key_pem
-  description = "The private SSH key for the Jenkins node"
-  sensitive   = true
-}
