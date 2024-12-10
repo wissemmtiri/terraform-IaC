cef916ed39c6474aa188559cd3226ece111b7c1a
Mtiri Wissem <wissem.mtiri@insat.ucar.tn>
2024-12-10 10:20:13 +0100

    remove monitoring vm

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ resource "azurerm_linux_web_app" "linux_webapp" {
 
     application_stack {
       docker_image_name = "nginx:latest"
+      # mtiriwissem/mytho-front:2.0
       docker_registry_url = "https://index.docker.io"
     }
   }
@@ -60,62 +61,62 @@ resource "azurerm_linux_web_app" "linux_webapp" {
 #================================================================================================
 #                                     MONITORING NODE
 #================================================================================================
-resource "azurerm_public_ip" "public-ip" {
-  name                = "public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-}
+# resource "azurerm_public_ip" "public-ip" {
+#   name                = "public-ip"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   allocation_method   = "Dynamic"
+#   sku                 = "Basic"
+# }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+# resource "azurerm_network_interface" "nic" {
+#   name                = "nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "nic-ip-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.public-ip.id
-  }
-}
+#   ip_configuration {
+#     name                          = "nic-ip-config"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.public-ip.id
+#   }
+# }
 
-resource "tls_private_key" "ssh" {
-  algorithm = var.ssh_key_algorithm
-  rsa_bits  = var.ssh_key_size
-}
+# resource "tls_private_key" "ssh" {
+#   algorithm = var.ssh_key_algorithm
+#   rsa_bits  = var.ssh_key_size
+# }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = "Monitoring-VM"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  size                  = var.vm_size
+# resource "azurerm_linux_virtual_machine" "vm" {
+#   name                  = "Monitoring-VM"
+#   location              = azurerm_resource_group.rg.location
+#   resource_group_name   = azurerm_resource_group.rg.name
+#   network_interface_ids = [azurerm_network_interface.nic.id]
+#   size                  = var.vm_size
 
-  os_disk {
-    name                 = "os-disk"
-    caching              = "ReadWrite"
-    storage_account_type = var.storage_account_type
-  }
+#   os_disk {
+#     name                 = "os-disk"
+#     caching              = "ReadWrite"
+#     storage_account_type = var.storage_account_type
+#   }
 
-  source_image_reference {
-    publisher = var.source_image_publisher
-    offer     = var.source_image_offer
-    sku       = var.source_image_sku
-    version   = "latest"
-  }
+#   source_image_reference {
+#     publisher = var.source_image_publisher
+#     offer     = var.source_image_offer
+#     sku       = var.source_image_sku
+#     version   = "latest"
+#   }
 
-  computer_name                   = "mon"
-  admin_username                  = "wess"
-  disable_password_authentication = true
+#   computer_name                   = "mon"
+#   admin_username                  = "wess"
+#   disable_password_authentication = true
 
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = tls_private_key.ssh.public_key_openssh
-  }
+#   admin_ssh_key {
+#     username   = var.admin_username
+#     public_key = tls_private_key.ssh.public_key_openssh
+#   }
 
-  tags = {
-    environment = var.environment
-  }
-}
+#   tags = {
+#     environment = var.environment
+#   }
+# }
